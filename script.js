204be09f6e1ae2860c3f63a4d6becf3a1071a634
Rose <rosebeats09@gmail.com>
2023-09-14 18:34:40 -0400

    Create buttons for player choice and style them

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+
+
 // create module for gameboard
 const gameBoard = (function() {
     const gameboard = ["x", "0"];
     return { gameboard };
 })();
 
-
+const showGameBoard = gameBoardFunc => {
+    const container = document.getElementById("container");
+    const square = document.querySelector("square")
+}
 // create factory function for players
 const player = (name) => {
-    return { name };
+    const getName = () => name;
+    const sayName = () => console.log(`${getName()} is the Player}.`);
+    return { getName, sayName };
 };
 const human = player('human');
 const computer = player('computer');
 console.log(human.name);
 console.log(computer.name);
+
+function getPlayerChoice() {
+    let id ="";
+    if (event.target.id == "x") {
+        console.log("Player chose X");
+    }
+    if(event.target.id == "zero") {
+         console.log("Player chose 0");
+    }
+}

ccf8e6befe5724ce8576918dbf6cd0f2cccd598f
Rose <rosebeats09@gmail.com>
2023-09-16 22:15:55 -0400

    Create img elements in DOM for x and 0

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ const gameBoard = (function() {
 
 const showGameBoard = gameBoardFunc => {
     const container = document.getElementById("container");
-    const square = document.querySelector("square");
+    
+
 }
+const square = document.getElementsByClassName("square");
+
+let img1 = document.createElement('img');
+img1.src = 'images/o.avif';
+img1.height = 60;
+img1.width = 60;
+
+
+let img2 = document.createElement('img');
+img2.src = 'images/x.avif';
+img2.height = 60;
+img2.width = 60;
+
+
 // create factory function for players
 const player = (name) => {
     const getName = () => name;
@@ -25,21 +40,23 @@ function getPlayerChoice () {
     const zero = document.getElementById("zero");
     const p = document.querySelector("p");
     const container = document.getElementById("container");
-    x.onclick = function () {
+    x.onclick = function (x) {
         p.style.display = "none";
         container.style.visibility = "visible";
         const playerChoice = "x";
         const computerChoice = "0";
         console.log("Player choice is x");
-        console.log("Player choice is 0");
+        console.log("Computer choice is 0");
     }
-    zero.onclick = function () {
+    zero.onclick = function (zero) {
         p.style.display = "none";
         container.style.visibility = "visible";
         const playerChoice = "0";
         const computerChoice = "x"
         console.log("Player choice is 0");
-        console.log("Player choice is x");
+        console.log("Computer choice is x");
     }
+    if (x) return x;
+    if (zero) return zero;
 }
 getPlayerChoice ();
